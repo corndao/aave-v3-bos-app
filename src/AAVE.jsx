d617cc1b952766e355e4b94acaa7d00284024da1
Robert Yan <mstr.raphael@gmail.com>
2023-05-31 16:53:05 +0800

    feat: import functions example

--- src/AAVE.jsx
+++ src/AAVE.jsx
@@ -1,1 +1,50 @@
-return <div>AAVE</div>;
+function getConfig(network) {
+  switch (network) {
+    case "mainnet":
+      return {
+        ownerId: "aave-v3.near",
+        nodeUrl: "https://rpc.mainnet.near.org",
+      };
+    case "testnet":
+      return {
+        ownerId: "aave-v3.testnet",
+        nodeUrl: "https://rpc.testnet.near.org",
+      };
+    default:
+      throw Error(`Unconfigured environment '${network}'.`);
+  }
+}
+const config = getConfig(context.networkId);
+
+State.init({
+  imports: null,
+});
+
+// Import functions to state.imports
+function onLoad(imports) {
+  if (!state.imports) {
+    State.update({
+      imports,
+    });
+  }
+}
+
+// Imported functions
+const { formatAmount } = state.imports;
+
+// Component body
+const body = !state.imports ? (
+  "Loading"
+) : (
+  <div>
+    <div>AAVE</div>
+    <div>Price: {formatAmount("1.001")}</div>
+  </div>
+);
+
+return (
+  <div>
+    <Widget src={`${config.ownerId}/widget/Utils.Number`} props={{ onLoad }} />
+    {body}
+  </div>
+);
---
+++ src/Utils/Number.jsx
@@ -0,0 +1,26 @@
+function isValid(a) {
+  if (!a) return false;
+  if (isNaN(Number(a))) return false;
+  if (a === "") return false;
+  return true;
+}
+
+function formatAmount(a) {
+  return isValid(a)
+    ? Number(a).toLocaleString(undefined, {
+        minimumFractionDigits: 5,
+        maximumFractionDigits: 5,
+      })
+    : a;
+}
+
+const { onLoad } = props;
+if (onLoad && typeof onLoad === "function") {
+  console.log("onLoad");
+  onLoad({
+    isValid,
+    formatAmount,
+  });
+}
+
+return <div style={{ display: "none" }} />;
