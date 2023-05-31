9280110a4ef22c89c64f23efe6c49fa1d0538652
Robert Yan <46699230+think-in-universe@users.noreply.github.com>
2023-05-31 19:54:58 +0800

    feat: import modules and functions (#2)

--- src/AAVE.jsx
+++ src/AAVE.jsx
@@ -1,1 +1,63 @@
-return <div>AAVE</div>;
+// App config
+function getConfig(network) {
+  switch (network) {
+    case "mainnet":
+      return {
+        ownerId: "aave-v3.near",
+        nodeUrl: "https://rpc.mainnet.near.org",
+      };
+    case "testnet":
+      return {
+        ownerId: "aave-v3.testnet",
+        nodeUrl: "https://rpc.testnet.near.org",
+      };
+    default:
+      throw Error(`Unconfigured environment '${network}'.`);
+  }
+}
+const config = getConfig(context.networkId);
+
+// App states
+State.init({
+  imports: null,
+});
+
+// Import functions to state.imports
+function importFunctions(imports) {
+  if (!state.imports) {
+    State.update({
+      imports,
+    });
+  }
+}
+
+// Define the modules you'd like to import
+const modules = [
+  `${config.ownerId}/widget/Utils.Number`,
+  `${config.ownerId}/widget/Utils.Date`,
+];
+// Import functions
+const { formatAmount, formatDateTime } = state.imports;
+
+// Component body
+const body = !state.imports ? (
+  "Loading..."
+) : (
+  <div>
+    <div>AAVE</div>
+    <div>Time: {formatDateTime(Date.now())}</div>
+    <div>Price: {formatAmount("1.001")}</div>
+  </div>
+);
+
+return (
+  <div>
+    {/* Component Head */}
+    <Widget
+      src={`${config.ownerId}/widget/Utils.Import`}
+      props={{ modules, onLoad: importFunctions }}
+    />
+    {/* Component Body */}
+    {body}
+  </div>
+);
---
+++ src/Utils/Import.jsx
@@ -0,0 +1,54 @@
+// `modules` are an array of components path who must have an `onLoad` callback props that return functions
+// `onLoad` needs to be defined in the parent component for receiving the loaded functions
+const { modules, onLoad } = props;
+
+// Helper function
+function hasCommonKeys(a, b) {
+  if (!a || !b) return false;
+  const commonKeys = Object.keys(a).filter((key) => key in b);
+  return commonKeys.length > 0;
+}
+
+State.init({
+  imports: null,
+  loadedModules: 0,
+});
+
+// Import functions from modules
+function importFunctions(imports) {
+  const loadedModules = state.loadedModules;
+  if (!state.imports) {
+    State.update({
+      imports,
+      loadedModules: loadedModules + 1,
+    });
+  } else if (!hasCommonKeys(state.imports, imports)) {
+    State.update({
+      imports: {
+        ...state.imports,
+        ...imports,
+      },
+      loadedModules: loadedModules + 1,
+    });
+  }
+  // invoke `onLoad` if all modules are ready
+  if (
+    state.loadedModules >= modules.length &&
+    onLoad &&
+    typeof onLoad === "function"
+  ) {
+    onLoad(state.imports);
+  }
+}
+
+const importedModules = !modules ? (
+  <div />
+) : (
+  <>
+    {modules.map((path) => (
+      <Widget src={path} props={{ onLoad: importFunctions }} />
+    ))}
+  </>
+);
+
+return <div style={{ display: "none" }}>{importedModules}</div>;
