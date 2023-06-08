d8b6d4bf00a2f396460b9d55fb83061eed58bee5
Steve Yu <steveyuowo@gmail.com>
2023-06-08 15:48:53 +0800

    feat: Web3Connect button

--- src/AAVE/Header.jsx
+++ src/AAVE/Header.jsx
@@ -3,6 +3,31 @@ const { config } = props;
 const Header = styled.div`
   padding: 18px 15px;
   background: #151718;
+
+  display: flex;
+  justify-content: space-between;
+
+  .web3-connect {
+    font-size: 12px;
+    font-weight: bold;
+
+    display: grid;
+    place-content: center;
+
+    background: #262626;
+    border-radius: 5px;
+    border: 0;
+
+    color: white;
+    transition: all 300ms ease-in-out;
+    &:hover {
+      background: #262626;
+      opacity: 0.5;
+    }
+    &:active {
+      background: #262626 !important;
+    }
+  }
 `;
 
 const AAVELogo = () => (
@@ -15,5 +40,6 @@ const AAVELogo = () => (
 return (
   <Header>
     <AAVELogo />
+    <Web3Connect className="web3-connect" connectLabel="Connect wallet" />
   </Header>
 );
