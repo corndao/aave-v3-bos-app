c9427a4f80bb1725648f6d3450ac2e0ce2920167
Steve Yu <steveyuowo@gmail.com>
2023-06-28 16:19:01 +0800

    fix: remaining debt

--- src/AAVE/Modal/RepayModal.jsx
+++ src/AAVE/Modal/RepayModal.jsx
@@ -421,21 +421,44 @@ return (
                     <Widget
                       src={`${config.ownerId}/widget/AAVE.Modal.FlexBetween`}
                       props={{
-                        left: <PurpleTexture>Debt</PurpleTexture>,
+                        left: <PurpleTexture>Remaining Debt</PurpleTexture>,
                         right: (
-                          <WhiteTexture>
-                            {Number(variableBorrows).toFixed(2)}
-                            <img
-                              src={`${config.ipfsPrefix}/bafkreiesqu5jyvifklt2tfrdhv6g4h6dubm2z4z4dbydjd6if3bdnitg7q`}
-                              width={16}
-                              height={16}
-                            />{" "}
-                            {isValid(state.amount)
-                              ? Big(variableBorrows)
-                                  .minus(state.amount)
-                                  .toFixed(2)
-                              : "-"}
-                          </WhiteTexture>
+                          <div style={{ textAlign: "right" }}>
+                            <WhiteTexture>
+                              {Number(variableBorrows).toFixed(7)}
+                              <img
+                                src={`${config.ipfsPrefix}/bafkreiesqu5jyvifklt2tfrdhv6g4h6dubm2z4z4dbydjd6if3bdnitg7q`}
+                                width={16}
+                                height={16}
+                              />{" "}
+                              {isValid(state.amount)
+                                ? Big(variableBorrows)
+                                    .minus(state.amount)
+                                    .toFixed(7)
+                                : "-"}
+                            </WhiteTexture>
+                            <WhiteTexture>
+                              {"$ " + isValid(variableBorrows) &&
+                              isValid(marketReferencePriceInUsd)
+                                ? Big(variableBorrows)
+                                    .times(marketReferencePriceInUsd)
+                                    .toFixed(2)
+                                : "-"}
+                              <img
+                                src={`${config.ipfsPrefix}/bafkreiesqu5jyvifklt2tfrdhv6g4h6dubm2z4z4dbydjd6if3bdnitg7q`}
+                                width={16}
+                                height={16}
+                              />{" "}
+                              {"$ " + isValid(state.amount) &&
+                              isValid(state.amount) &&
+                              isValid(marketReferencePriceInUsd)
+                                ? Big(variableBorrows)
+                                    .minus(state.amount)
+                                    .times(marketReferencePriceInUsd)
+                                    .toFixed(2)
+                                : "-"}
+                            </WhiteTexture>
+                          </div>
                         ),
                       }}
                     />
