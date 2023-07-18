0fd6088328b728c42158a97bcdb5fcfea9a827a5
Steve Yu <steveyuowo@gmail.com>
2023-07-18 16:20:20 +0800

    fix: support borrowing enabled

--- src/AAVE.jsx
+++ src/AAVE.jsx
@@ -38,7 +38,6 @@ function getNetworkConfig(chainId) {
         aavePoolV3Address: "0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2",
         wrappedTokenGatewayV3Address:
           "0xD322A49006FC828F9B5B37Ab215F99B4E5caB19C",
-        borrowBlackListToken: ["AAVE"],
         ...abis,
         ...constants,
       };
@@ -50,7 +49,6 @@ function getNetworkConfig(chainId) {
         aavePoolV3Address: "0x794a61358D6845594F94dc1DB02A252b5b4814aD",
         wrappedTokenGatewayV3Address:
           "0xB5Ee21786D28c5Ba61661550879475976B707099",
-        borrowBlackListToken: ["AAVE"],
         ...abis,
         ...constants,
       };
@@ -62,7 +60,6 @@ function getNetworkConfig(chainId) {
         aavePoolV3Address: "0x794a61358D6845594F94dc1DB02A252b5b4814aD",
         wrappedTokenGatewayV3Address:
           "0x1e4b7A6b903680eab0c5dAbcb8fD429cD2a9598c",
-        borrowBlackListToken: ["AAVE"],
         ...abis,
         ...constants,
       };
@@ -74,7 +71,6 @@ function getNetworkConfig(chainId) {
         aavePoolV3Address: "0x4412c92f6579D9FC542D108382c8D1d6D2Be63d9",
         wrappedTokenGatewayV3Address:
           "0xD82940E16D25aB1349914e1C369eF1b287d457BF",
-        borrowBlackListToken: ["AAVE"],
         ...abis,
         ...constants,
       };
@@ -625,11 +621,9 @@ function updateUserDebts(marketsMapping, assetsToSupply) {
             return availableBorrowsUSD2 - availableBorrowsUSD1;
           return asset1.symbol.localeCompare(asset2.symbol);
         })
-        .filter(
-          (asset) =>
-            !config.borrowBlackListToken ||
-            !config.borrowBlackListToken.includes(asset.symbol)
-        ),
+        .filter((asset) => {
+          return asset.borrowingEnabled;
+        }),
     };
     const yourBorrows = {
       ...assetsToBorrow,
